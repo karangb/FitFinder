d2d1be197d9443a83f87b25e0ff31e9efb76d0e4
Karan G <karangb@gmail.com>
2013-01-26 18:26:10 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitFinder::Application.config.secret_token = '79ae4e91487b1dcc7988d9a54741bf24ea1795461b62b285d1a2f51f1c84b759a95e7a7496adb47d82fb41e8251756e9d3b61aee7bad3d44ebef8fcb7a48e10f'
